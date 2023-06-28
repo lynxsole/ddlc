b34f35baa209fa3fa68aa6964044836bef9943d5
Lynxsole <john1880@outlook.fr>
2023-06-29 01:24:53 +0200

    new file:   assets/content/font/Bagel_Fat_One.zip 	new file:   assets/content/img/background/BG index.png 	new file:   assets/content/img/character/Yuri.png 	new file:   assets/content/img/character/monika.png 	new file:   assets/content/img/character/natsuki.png 	new file:   assets/content/img/character/sayori.png 	new file:   assets/content/img/logo/game-logo.png 	new file:   assets/content/img/ref/Menu-1-1-1.png 	new file:   assets/css/index.css 	new file:   index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <title>DDLC.fan</title>
+</head>
+<body>
+    <section class="top">
+        <div class="content">
+            <div class="titleCircle">
+                <img class="titleCircle" src="assets\content\img\logo\game-logo.png" alt="" srcset="">
+            </div>
+            <div class="cicle">
+                <div class="homeBtn">
+                    <li class="btn">Home</li>
+                    <li class="btn">Story</li>
+                    <li class="btn">Screen</li>
+                    <li class="btn">About</li>
+                </div>
+            </div>
+
+            <div class="character">
+                <div class="natsuki">
+                    <img src="assets\content\img\character\sayori.png" alt="natsuki">
+                </div>
+                <div class="yuri">
+                    <img src="assets\content\img\character\Yuri.png" alt="yuri">
+                </div>
+                <div class="sayori">
+                    <img src="assets\content\img\character\natsuki.png" alt="yuri">
+                </div>
+                <div class="monika" class="monika">
+                    <img src="assets\content\img\character\monika.png" alt="yuri">
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
